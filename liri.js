a32c924ff7fa52c44d425086482464d068bc43b6
ndgithub <nickydesai.mail@gmail.com>
2019-02-27 08:32:25 -0600

    Adds functionality for 'do what it says'

--- liri.js
+++ liri.js
@@ -3,149 +3,142 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var moment = require('moment')
+var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var searchTerm = process.argv[3];
 
-switch (command) {
+makeRequest(command, searchTerm);
+
+function makeRequest(request, query) {
+  switch (request) {
     case 'spotify-this-song':
-        getSpotifyData(searchTerm);
-        break;
+      getSpotifyData(query);
+      break;
     case 'concert-this':
-        getConcertData(searchTerm);
-        break;
+      getConcertData(query);
+      break;
     case 'movie-this':
-        getMovie(searchTerm);
-        break;
+      getMovie(query);
+      break;
     case 'do-what-it-says':
-        break;
+      doWhatItSays();
+      break;
+  }
 }
 
+function doWhatItSays() {
+  fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) { console.log(err) };
+    var data = data.split(',');
+    makeRequest(data[0], data[1])
+  });
+}
 
 function getMovie(movie) {
-    axios.get("http://www.omdbapi.com/?t=" + movie + "&apiKey=trilogy"
-    )
-        .then(function (response) {
-            console.log();
-            var title = response.data.Title;
-            var year = response.data.Year;
-            var imdb = response.data.imdbRating;
-            // var rotTom = response.data
-            var country = response.data.Country;
-            var language = response.data.Language;
-            var plot = response.data.Plot;
-            var actors = response.data.Actors;
-            //
-            var info = {
-                title: {
-                    text: title,
-                    label: 'Title: '
-                },
-                year: {
-                    text: year,
-                    label: 'Release Year : '
-                },
-                imdb: {
-                    text: imdb,
-                    label: 'IMDB Rating: '
-                },
-                country: {
-                    text: country,
-                    label: 'Country: ',
-                },
-                language: {
-                    text: language,
-                    label: 'Language : ',
-                },
-                plot: {
-                    text: plot,
-                    label: 'Plot: ',
-                },
-                actors: {
-                    text: actors,
-                    label: 'Actors: ',
-                }
-            };
-            updateUI(info);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+  axios.get("http://www.omdbapi.com/?t=" + movie + "&apiKey=trilogy"
+  )
+    .then(function (response) {
+
+      var info = {
+        title: {
+          text: response.data.Title,
+          label: 'Title: '
+        },
+        year: {
+          text: response.data.Year,
+          label: 'Release Year : '
+        },
+        imdb: {
+          text: response.data.imdbRating,
+          label: 'IMDB Rating: '
+        },
+        country: {
+          text: response.data.Country,
+          label: 'Country: ',
+        },
+        language: {
+          text: response.data.Language,
+          label: 'Language : ',
+        },
+        plot: {
+          text: response.data.Plot,
+          label: 'Plot: ',
+        },
+        actors: {
+          text: response.data.Actors,
+          label: 'Actors: ',
+        }
+      };
+      updateUI(info);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
 }
-
-
 
 function getConcertData(artist) {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    )
-        .then(function (response) {
-            var venueName = response.data[0].venue.name;
-            var venueLocation = response.data[0].venue.region;
-            var date = response.data[0].datetime;
-            var formattedDate = moment(date).format('MM/DD/YYYY');
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+  )
+    .then(function (response) {
+      var date = response.data[0].datetime;
+      var formattedDate = moment(date).format('MM/DD/YYYY');
 
-            var info = {
-                venueName: {
-                    text: venueName,
-                    label: 'Venue: '
-                },
-                venueLocation: {
-                    text: venueLocation,
-                    label: 'Venue Location: '
-                },
-                date: {
-                    text: formattedDate,
-                    label: 'Date: ',
-                }
-            };
-            updateUI(info);
-        })
-        .catch(function (error) {
-            console.log('error');
-        });
+      var info = {
+        venueName: {
+          text: response.data[0].venue.name,
+          label: 'Venue: '
+        },
+        venueLocation: {
+          text: response.data[0].venue.region,
+          label: 'Venue Location: '
+        },
+        date: {
+          text: formattedDate,
+          label: 'Date: ',
+        }
+      };
+      updateUI(info);
+    })
+    .catch(function (error) {
+      console.log('error');
+    });
 
 }
 
-
-
-
 function getSpotifyData(searchTerm) {
-    var info = {};
-    spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        var artistName = data.tracks.items[0].album.artists[0].name;
-        var songTitle = data.tracks.items[0].name;
-        var previewLink = data.tracks.items[0].external_urls.spotify;
-        var albumName = data.tracks.items[0].album.name;
-        info = {
-            artistName: {
-                text: artistName,
-                label: 'Artist Name: '
-            },
-            songTitle: {
-                text: songTitle,
-                label: 'Song Title: '
-            },
-            previewLink: {
-                text: previewLink,
-                label: 'Preview: '
-            },
-            albumName: {
-                text: albumName,
-                label: 'Album Name: '
-            },
-        };
-        updateUI(info);
-    });
+  var info = {};
+  spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    info = {
+      artistName: {
+        text: data.tracks.items[0].album.artists[0].name,
+        label: 'Artist Name: '
+      },
+      songTitle: {
+        text: data.tracks.items[0].name,
+        label: 'Song Title: '
+      },
+      previewLink: {
+        text: data.tracks.items[0].external_urls.spotify,
+        label: 'Preview: '
+      },
+      albumName: {
+        text: data.tracks.items[0].album.name,
+        label: 'Album Name: '
+      },
+    };
+    updateUI(info);
+  });
 }
 
 function updateUI(infoObject) {
-    for (var key in infoObject) {
-        console.log(infoObject[key].label + infoObject[key].text);
-    }
+  for (var key in infoObject) {
+    console.log(infoObject[key].label + infoObject[key].text);
+  }
 }
